60ff086b8e40ae9afdac725d7209925fe1f8b7ef
oca <oCa.flash@gmail.com>
2025-12-12 17:10:38 +0300

    add kinematics

---
+++ src/crab_body_kinematics/src/body_kinematics.cpp
@@ -0,0 +1,216 @@
+#include "body_kinematics.hpp"
+
+using namespace std::chrono_literals;
+
+const static std::string suffixes[6] = {"_r1", "_r2", "_r3", "_l1", "_l2", "_l3"};
+
+BodyKinematics::BodyKinematics() : Node("body_kinematics") {
+    // Declare parameters
+    this->declare_parameter<std::string>("robot_description", "");
+    this->declare_parameter<std::string>("root_name_body", "leg_center");
+    this->declare_parameter<std::string>("tip_name_body", "coxa");
+    this->declare_parameter<double>("clearance", 0.045);
+}
+
+bool BodyKinematics::init() {
+    std::string robot_desc_string;
+    
+    // Get URDF XML
+    robot_desc_string = this->get_parameter("robot_description").as_string();
+    if (robot_desc_string.empty()) {
+        RCLCPP_FATAL(this->get_logger(), "Could not load the xml from parameter: robot_description");
+        return false;
+    }
+
+    // Get Root and Tip From Parameter Server
+    root_name_ = this->get_parameter("root_name_body").as_string();
+    tip_name_ = this->get_parameter("tip_name_body").as_string();
+    z_ = this->get_parameter("clearance").as_double();
+
+    // Load and Read Models
+    if (!loadModel(robot_desc_string)) {
+        RCLCPP_FATAL(this->get_logger(), "Could not load models!");
+        return false;
+    }
+
+    client_ = this->create_client<crab_msgs::srv::GetLegIKSolver>("/crab_leg_kinematics/get_ik");
+    joints_pub_ = this->create_publisher<crab_msgs::msg::LegsJointsState>("joints_to_controller", 1);
+    body_move_sub_ = this->create_subscription<crab_msgs::msg::BodyState>(
+        "/teleop/move_body", 1,
+        std::bind(&BodyKinematics::teleopBodyMove, this, std::placeholders::_1));
+    body_cmd_sub_ = this->create_subscription<crab_msgs::msg::BodyCommand>(
+        "/teleop/body_command", 1,
+        std::bind(&BodyKinematics::teleopBodyCmd, this, std::placeholders::_1));
+
+    bs_.leg_radius = 0.11;
+    bs_.z = -0.016;
+    
+    std::this_thread::sleep_for(1s);
+    if (calculateKinematics(&bs_)) {
+        joints_pub_->publish(legs_);
+    }
+    RCLCPP_INFO(this->get_logger(), "Ready to receive teleop messages...");
+
+    return true;
+}
+
+bool BodyKinematics::loadModel(const std::string& xml) {
+    // Construct tree with kdl_parser
+    KDL::Tree tree;
+
+    if (!kdl_parser::treeFromString(xml, tree)) {
+        RCLCPP_ERROR(this->get_logger(), "Could not initialize tree object");
+        return false;
+    }
+    RCLCPP_INFO(this->get_logger(), "Construct tree");
+
+    // Get coxa and leg_center frames via segments
+    std::map<std::string, KDL::TreeElement>::const_iterator segments_iter;
+    std::string link_name_result;
+    for (int i = 0; i < num_legs_; i++) {
+        link_name_result = root_name_ + suffixes[i];
+        segments_iter = tree.getSegment(link_name_result);
+        frames_.push_back((*segments_iter).second.segment.getFrameToTip());
+    }
+    for (int i = 0; i < num_legs_; i++) {
+        link_name_result = tip_name_ + suffixes[i];
+        segments_iter = tree.getSegment(link_name_result);
+        frames_.push_back((*segments_iter).second.segment.getFrameToTip());
+    }
+    RCLCPP_INFO(this->get_logger(), "Get frames");
+
+    // Vector iterators
+    for (size_t i = 0; i < num_legs_; i++) {
+        frames_[i] = frames_[i] * frames_[i + num_legs_];
+    }
+    frames_.resize(num_legs_);
+
+    for (size_t i = 0; i < num_legs_; i++) {
+        for (size_t j = 0; j < num_joints_; j++) {
+            legs_.joints_state[i].joint[j] = 0;
+        }
+    }
+
+    return true;
+}
+
+bool BodyKinematics::calculateKinematics(crab_msgs::msg::BodyState* body_ptr) {
+    // Body rotation
+    rotation_ = KDL::Rotation::RPY(body_ptr->roll, body_ptr->pitch, body_ptr->yaw);
+
+    // Distance from body center to leg tip
+    femur_frame_ = KDL::Frame(KDL::Vector(body_ptr->leg_radius, 0, 0));
+
+    // Offset from center
+    offset_vector_ = KDL::Vector(body_ptr->x, body_ptr->y, body_ptr->z);
+    rotate_correction_ = KDL::Vector(
+        body_ptr->z * tan(body_ptr->pitch),
+        -(body_ptr->z * tan(body_ptr->roll)),
+        0);
+
+    for (size_t i = 0; i < num_legs_; i++) {
+        // Get tip frames
+        tibia_foot_frame_ = frames_[i] * femur_frame_;
+        // Get tip vectors with body position
+        final_vector_[i] = (rotation_ * tibia_foot_frame_.p) + offset_vector_ + rotate_correction_;
+    }
+
+    if (!callService(final_vector_)) {
+        return false;
+    }
+
+    return true;
+}
+
+bool BodyKinematics::callService(KDL::Vector* vector) {
+    auto request = std::make_shared<crab_msgs::srv::GetLegIKSolver::Request>();
+    
+    // Creating message to request
+    for (size_t i = 0; i < num_legs_; i++) {
+        request->leg_number.push_back(i);
+        crab_msgs::msg::LegPositionState leg_pos_buf;
+        leg_pos_buf.x = vector[i].x();
+        leg_pos_buf.y = vector[i].y();
+        leg_pos_buf.z = vector[i].z();
+        request->target_position.push_back(leg_pos_buf);
+        request->current_joints.push_back(legs_.joints_state[i]);
+    }
+
+    // Wait for service
+    if (!client_->wait_for_service(1s)) {
+        RCLCPP_WARN(this->get_logger(), "Service not available");
+        return false;
+    }
+
+    auto future = client_->async_send_request(request);
+    
+    // Wait for result
+    if (rclcpp::spin_until_future_complete(this->get_node_base_interface(), future, 1s) ==
+        rclcpp::FutureReturnCode::SUCCESS) {
+        auto response = future.get();
+        if (response->error_codes == crab_msgs::srv::GetLegIKSolver::Response::IK_FOUND) {
+            for (size_t i = 0; i < num_legs_; i++) {
+                for (size_t j = 0; j < num_joints_; j++) {
+                    legs_.joints_state[i].joint[j] = response->target_joints[i].joint[j];
+                }
+            }
+        } else {
+            RCLCPP_ERROR(this->get_logger(), "An IK solution could not be found");
+            return false;
+        }
+    } else {
+        RCLCPP_ERROR(this->get_logger(), "Failed to call service");
+        return false;
+    }
+    return true;
+}
+
+void BodyKinematics::teleopBodyMove(const crab_msgs::msg::BodyState::SharedPtr body_state) {
+    bs_.x = body_state->x;
+    bs_.y = body_state->y;
+    bs_.z = body_state->z;
+    bs_.pitch = body_state->pitch;
+    bs_.roll = body_state->roll;
+    bs_.yaw = body_state->yaw;
+    bs_.leg_radius = body_state->leg_radius;
+    if (calculateKinematics(&bs_)) {
+        joints_pub_->publish(legs_);
+    }
+}
+
+void BodyKinematics::teleopBodyCmd(const crab_msgs::msg::BodyCommand::SharedPtr body_cmd) {
+    if (body_cmd->cmd == crab_msgs::msg::BodyCommand::STAND_UP_CMD) {
+        RCLCPP_INFO(this->get_logger(), "STAND_UP_CMD");
+        rclcpp::Rate r(25);
+        while (bs_.z >= -z_) {
+            bs_.z -= 0.0025;
+            r.sleep();
+            if (calculateKinematics(&bs_)) {
+                joints_pub_->publish(legs_);
+            }
+        }
+    }
+    if (body_cmd->cmd == crab_msgs::msg::BodyCommand::SEAT_DOWN_CMD) {
+        RCLCPP_INFO(this->get_logger(), "SEAT_DOWN_CMD");
+        rclcpp::Rate r(25);
+        while (bs_.z <= -0.016) {
+            bs_.z += 0.0025;
+            r.sleep();
+            if (calculateKinematics(&bs_)) {
+                joints_pub_->publish(legs_);
+            }
+        }
+    }
+}
+
+int main(int argc, char **argv) {
+    rclcpp::init(argc, argv);
+    auto node = std::make_shared<BodyKinematics>();
+    if (!node->init()) {
+        RCLCPP_ERROR(node->get_logger(), "Could not initialize kinematics node");
+        return -1;
+    }
+    rclcpp::spin(node);
+    rclcpp::shutdown();
+    return 0;
+}
---
+++ src/crab_body_kinematics/src/body_kinematics.hpp
@@ -0,0 +1,45 @@
+#ifndef BODY_KINEMATICS_HPP_
+#define BODY_KINEMATICS_HPP_
+
+#include <rclcpp/rclcpp.hpp>
+#include <kdl_parser/kdl_parser.hpp>
+#include <kdl/frames.hpp>
+#include <crab_msgs/srv/get_leg_ik_solver.hpp>
+#include <crab_msgs/msg/legs_joints_state.hpp>
+#include <crab_msgs/msg/body_state.hpp>
+#include <crab_msgs/msg/body_command.hpp>
+
+#define NUM_LEGS 6
+#define NUM_JOINTS 3
+
+class BodyKinematics : public rclcpp::Node {
+public:
+    BodyKinematics();
+    bool init();
+
+private:
+    std::string root_name_, tip_name_;
+    std::vector<KDL::Frame> frames_;
+    crab_msgs::msg::BodyState bs_;
+    crab_msgs::msg::LegsJointsState legs_;
+    static constexpr unsigned int num_joints_ = NUM_JOINTS;
+    static constexpr unsigned int num_legs_ = NUM_LEGS;
+    double z_;
+
+    KDL::Rotation rotation_;
+    KDL::Frame tibia_foot_frame_, femur_frame_;
+    KDL::Vector offset_vector_, rotate_correction_, final_vector_[NUM_LEGS];
+
+    rclcpp::Client<crab_msgs::srv::GetLegIKSolver>::SharedPtr client_;
+    rclcpp::Publisher<crab_msgs::msg::LegsJointsState>::SharedPtr joints_pub_;
+    rclcpp::Subscription<crab_msgs::msg::BodyState>::SharedPtr body_move_sub_;
+    rclcpp::Subscription<crab_msgs::msg::BodyCommand>::SharedPtr body_cmd_sub_;
+
+    bool loadModel(const std::string& xml);
+    bool calculateKinematics(crab_msgs::msg::BodyState* body_ptr);
+    bool callService(KDL::Vector* vector);
+    void teleopBodyMove(const crab_msgs::msg::BodyState::SharedPtr body_state);
+    void teleopBodyCmd(const crab_msgs::msg::BodyCommand::SharedPtr body_cmd);
+};
+
+#endif /* BODY_KINEMATICS_HPP_ */
