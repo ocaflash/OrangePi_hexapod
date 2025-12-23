42e60e334fb19e6927ef729541585a4ae27087cc
oca <oCa.flash@gmail.com>
2025-12-23 23:34:43 +0300

    fix

--- src/crab_description/launch/robot.launch.py
+++ src/crab_description/launch/robot.launch.py
@@ -4,6 +4,7 @@ from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
 from launch_ros.actions import Node
 from launch.substitutions import Command, LaunchConfiguration, PythonExpression
+from launch_ros.parameter_descriptions import ParameterValue
 from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
@@ -31,10 +32,13 @@ def generate_launch_description():
         ]),
     ]
 
-    robot_description = Command([
-        'xacro ', xacro_file,
-        *xacro_args
-    ])
+    robot_description = ParameterValue(
+        Command([
+            'xacro ', xacro_file,
+            *xacro_args
+        ]),
+        value_type=str
+    )
 
     return LaunchDescription([
         use_primitives_arg,
